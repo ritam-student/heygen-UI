885ba56e06602c58d4ef00a9bfc8787be6f91d2e
unknown <ritamacharya2004@gmail.com>
2025-05-24 19:59:41 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black text-white h-screen w-full flex items-center justify-center'>
+      hi there
+    </div>
+  )
+}
+
+export default App
